b20f8f2515b1cddd37054f8c3117782f6f3d9681
BackRunner <12205593+backrunner@users.noreply.github.com>
2020-05-16 17:04:16 +0800

    add: element

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue';
 import App from './App.vue';
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import router from './router';
+import './styles/main.less';
+import './plugins/element.js'
 
 Vue.config.productionTip = false;
+
+// set up axios
+Vue.use(VueAxios, axios)
 
 new Vue({
     router,
