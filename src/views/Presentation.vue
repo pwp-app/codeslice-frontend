4a120a425a415d96f6db3a5cc588735cfffad065
BackRunner <12205593+backrunner@users.noreply.github.com>
2020-05-18 02:41:28 +0800

    add: error message & retry button

--- src/views/Presentation.vue
+++ src/views/Presentation.vue
@@ -9,11 +9,16 @@
                         <span>加载中</span>
                     </div>
                 </div>
-                <div class="container present-main" v-if="slice.content && !slice.loading">
+                <div
+                    class="container present-main"
+                    v-if="slice.content && !slice.loading"
+                >
                     <Header></Header>
                     <div class="present-text present-title">
                         <span>
-                            以下是<i class="present-space"></i>{{ slice.poster ? slice.poster : '' }}<i class="present-space"></i>分享给您的代码：
+                            以下是<i class="present-space"></i
+                            >{{ slice.poster ? slice.poster : ""
+                            }}<i class="present-space"></i>分享给您的代码：
                         </span>
                     </div>
                     <div class="present-code">
@@ -22,23 +27,35 @@
                     <div class="present-bottom">
                         <div class="present-info">
                             <div class="present-info-create">
-                                <span>创建时间：{{slice.createTime}}</span>
+                                <span>创建时间：{{ slice.createTime }}</span>
                             </div>
                             <div class="present-info-expires">
-                                <span>过期时间：{{slice.expires}}</span>
+                                <span>过期时间：{{ slice.expires }}</span>
                             </div>
                         </div>
                         <div class="present-action">
-                            <el-button type="primary" @click="doCopy">复制到剪贴板</el-button>
-                            <el-button type="primary" @click="doSave">下载为文本</el-button>
+                            <el-button type="primary" @click="doCopy"
+                                >复制到剪贴板</el-button
+                            >
+                            <el-button type="primary" @click="doSave"
+                                >下载为文本</el-button
+                            >
                         </div>
                         <div class="clearfix"></div>
                     </div>
                 </div>
-                <div class="container present-main" v-if="!slice.content && !slice.loading">
+                <div
+                    class="container present-main"
+                    v-if="!slice.content && !slice.loading"
+                >
                     <Header></Header>
                     <div class="present-text present-error">
-                        <span>很抱歉，该代码不存在或已过期。</span>
+                        <span>{{
+                            error ? error : "很抱歉，该代码不存在或已过期。"
+                        }}</span>
+                    </div>
+                    <div class="present-retry">
+                        <el-button type="primary">重试</el-button>
                     </div>
                 </div>
             </transition>
@@ -48,8 +65,8 @@
 </template>
 
 <script>
-import dayjs from 'dayjs';
-import saveAs from 'file-saver';
+import dayjs from "dayjs";
+import saveAs from "file-saver";
 import Header from "../components/header";
 import Footer from "../components/footer";
 import "../styles/views/presentation.less";
@@ -64,51 +81,68 @@ export default {
         return {
             slice: {
                 loading: true
-            }
+            },
+            error: null,
+            timeout: false
         };
     },
     created() {
-        this.axios
-            .get("/api/v1/slice/get", {
-                params: {
-                    key: this.$route.params.key
-                }
-            })
-            .then(res => {
-                if (res && res.data.code === 200) {
-                    if (res.data.data.createTime + res.data.data.expires <= new Date().getTime() / 1000) {
-                        this.slice.loading = false;
-                        return;
-                    }
-                    this.slice = res.data.data;
-                    this.slice.createTime = dayjs(this.slice.createTime).format('YYYY-MM-DD HH:mm:ss');
-                    this.slice.expires = dayjs(this.slice.createTime).add(this.slice.expires, 's').format('YYYY-MM-DD HH:mm:ss');
-                    if (this.slice.content) {
-                        this.$nextTick(() => {
-                            let code = document.getElementById('code');
-                            hljs.highlightBlock(code);
-                            hljs.lineNumbersBlock(code);
-                        });
-                    }
-                }
-            }).catch(() => {
-                this.slice.loading = false;
-                this.slice.content = null;
-            });
+        this.fetch();
     },
     methods: {
+        fetch() {
+            this.axios
+                .get("/api/v1/slice/get", {
+                    params: {
+                        key: this.$route.params.key
+                    }
+                })
+                .then(res => {
+                    if (res && res.data.code === 200) {
+                        if (
+                            res.data.data.createTime + res.data.data.expires <=
+                            new Date().getTime() / 1000
+                        ) {
+                            this.slice.loading = false;
+                            return;
+                        }
+                        this.slice = res.data.data;
+                        this.slice.createTime = dayjs(
+                            this.slice.createTime
+                        ).format("YYYY-MM-DD HH:mm:ss");
+                        this.slice.expires = dayjs(this.slice.createTime)
+                            .add(this.slice.expires, "s")
+                            .format("YYYY-MM-DD HH:mm:ss");
+                        if (this.slice.content) {
+                            this.$nextTick(() => {
+                                let code = document.getElementById("code");
+                                hljs.highlightBlock(code);
+                                hljs.lineNumbersBlock(code);
+                            });
+                        }
+                    }
+                })
+                .catch(() => {
+                    this.slice.loading = false;
+                    this.slice.content = null;
+                    this.error = "获取失败，请检查您的网络设置。";
+                });
+        },
         doCopy() {
-            this.$copyText(this.slice.content).then(e => {
-                this.$message({
-                    type: 'success',
-                    message: '代码已复制到剪贴板'
-                });
-            }, function (e) {
-                this.$message({
-                    type: 'error',
-                    message: '复制失败'
-                });
-            })
+            this.$copyText(this.slice.content).then(
+                e => {
+                    this.$message({
+                        type: "success",
+                        message: "代码已复制到剪贴板"
+                    });
+                },
+                function(e) {
+                    this.$message({
+                        type: "error",
+                        message: "复制失败"
+                    });
+                }
+            );
         },
         doSave() {
             const filename = `${this.$route.params.key}.txt`;
