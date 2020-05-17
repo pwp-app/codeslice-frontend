328ea37a65c8cc5977a3e6c7041f76de0b48729f
BackRunner <12205593+backrunner@users.noreply.github.com>
2020-05-18 00:48:57 +0800

    add: 404 page

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,12 +1,16 @@
 <template>
-    <div class="container-404">
-        <div class="404-text">
+    <div class="container-notfound">
+        <div class="notfound-text">
             <p>404 not found</p>
             <p>这里什么也没有</p>
         </div>
-        <div class="404-action">
+        <div class="notfound-action">
             <el-button type="primary" @click="goBack">返回之前的页面</el-button>
         </div>
+        <footer class="notfound-footer">
+            <img :src="logo">
+            <span>CodeSlice</span>
+        </footer>
     </div>
 </template>
 
@@ -14,6 +18,11 @@
 import '../styles/views/404.less';
 export default {
     name: '404',
+    data() {
+        return {
+            logo: require('../../assets/codeslice.png')
+        }
+    },
     methods: {
         goBack() {
             window.history.go(-1);
