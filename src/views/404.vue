c31c8d6296e6719740fb28985aea5e774c47235c
BackRunner <12205593+backrunner@users.noreply.github.com>
2020-05-17 21:13:53 +0800

    add: 404

---
+++ src/views/404.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container-404">
+        <div class="404-text">
+            <p>404 not found</p>
+            <p>这里什么也没有</p>
+        </div>
+        <div class="404-action">
+            <el-button type="primary" @click="goBack">返回之前的页面</el-button>
+        </div>
+    </div>
+</template>
+
+<script>
+import '../styles/views/404.less';
+export default {
+    name: '404',
+    methods: {
+        goBack() {
+            window.history.go(-1);
+        }
+    }
+}
+</script>
